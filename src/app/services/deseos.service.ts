2f7689d846741946d12be9962325c7ed452bd303
Alex Lopez <alex2761086444@gmail.com>
2020-12-01 09:06:39 -0600

    Pantalla de pendeientes- Diseño ydocumentacion de ionic

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,11 @@ export class DeseosService {
   listas: any [] = [];
 
   constructor() {
-    console.log('Servicio inicializado');
+
+    const lista1 = new Lista('Recolectar pidras del infinito');
+    const lista2 = new Lista('Heroes adesaparecer');
+
+    this.listas.push(lista1, lista2);
+
   }
 }
