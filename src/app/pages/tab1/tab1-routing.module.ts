b695f0299798dd60e7d47bf48cfbe275cac17aa2
Alex Lopez <alex2761086444@gmail.com>
2020-12-01 09:38:49 -0600

    Navegacion entre pantallas

--- src/app/pages/tab1/tab1-routing.module.ts
+++ src/app/pages/tab1/tab1-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     path: '',
     component: Tab1Page,
   }
+  {
+    path: 'agregar',
+    loadChildren: () => import('../agregar/agregar.module').then( m => m.AgregarPageModule)
+  }
 ];
 
 @NgModule({
